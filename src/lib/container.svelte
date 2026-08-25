465746374e01c906440c9883a2f455391cadb3da
mitchsaylor <mitch@saylor.io>
2026-08-25 09:13:33 -0400

    add components

---
+++ src/lib/container.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	let { children, size = 'md' }: { children?: Snippet; size?: 'sm' | 'md' | 'lg' } = $props();
+
+	// Types
+	import type { Snippet } from 'svelte';
+</script>
+
+<div class="px-[3%] py-8">
+	{#if children}
+		{@render children()}
+	{/if}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,33 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Container from '#lib/container.svelte';
+
+	const textSizes: string[] = [
+		'text-3xs',
+		'text-2xs',
+		'text-xs',
+		'text-sm',
+		'text-md',
+		'text-base',
+		'text-lg',
+		'text-xl',
+		'text-2xl',
+		'text-3xl',
+		'text-4xl',
+		'text-5xl',
+		'text-6xl',
+		'text-7xl'
+	];
+</script>
+
+<Container>
+	<h1 class="text-4xl font-bold">WindWeave UI</h1>
+	<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p>
+</Container>
+<Container>
+	{#each textSizes as size (size)}
+		<p class={size}>{size}</p>
+	{/each}
+</Container>
